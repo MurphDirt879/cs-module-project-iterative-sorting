f73aa05c2d506e579822452c3b39aa9a9747957a
MurphDirt879 <boxert879@hotmail.com>
2020-05-19 15:16:21 -0400

    Finsihed

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -34,6 +34,37 @@ def bubble_sort(arr):
 # STRETCH: implement the Count Sort function below
 def count_sort(arr, maximum=-1):
     # Your code here
+     
+    # Count the number of times each value appears.
+    # counts[0] stores the number of 0's in the input
+    # counts[4] stores the number of 4's in the input
+    # etc.
+    counts = [0] * (maximum + 1)
+    for item in arr:
+        counts[item] += 1
 
+    # Overwrite counts to hold the next index an item with
+    # a given value goes. So, counts[4] will now store the index
+    # where the next 4 goes, not the number of 4's our
+    # list has.
+    num_items_before = 0
+    for i, count in enumerate(counts):
+        counts[i] = num_items_before
+        num_items_before += count
+
+    # Output list to be filled in
+    sorted_list = [None] * len(arr)
+
+    # Run through the input list
+    for item in arr:
+        
+        # Place the item in the sorted list
+        sorted_list[ counts[item] ] = item
+
+        # And, make sure the next item we see with the same value
+        # goes after the one we just placed
+        counts[item] += 1
+
+    arr = sorted_list
 
     return arr
